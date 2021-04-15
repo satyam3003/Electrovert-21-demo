c0a95f5ba8dfb5123cdc922040134ff1cff6d651
satyam3003 <75485302+satyam3003@users.noreply.github.com>
2021-04-16 04:31:29 +0530

    v1.1

---
+++ index.js
@@ -0,0 +1,17 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml10 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml10 .letter',
+    rotateY: [-120, 0],
+    duration: 1300,
+    delay: (el, i) => 60 * i
+  }).add({
+    targets: '.ml10',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 10000
+  });
