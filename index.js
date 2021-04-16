883b426d81a789c5644d333a6cb1911ce6ae63ad
satyam3003 <75485302+satyam3003@users.noreply.github.com>
2021-04-16 14:32:59 +0530

    V2.0

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='let
 anime.timeline({loop: true})
   .add({
     targets: '.ml10 .letter',
-    rotateY: [-120, 0],
+    rotateY: [-90, 0],
     duration: 1300,
     delay: (el, i) => 60 * i
   }).add({
